596989f980b0ee09eab85df17550c5e4364817b4
Thiago <tfrancar@gmial.com>
2023-08-13 18:54:30 -0300

    Subindo alterações

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/tfrancar/meu-primeiro-crud-go
+
+go 1.18
+
+require github.com/joho/godotenv v1.5.1 // indirect
---
+++ src/configuration/rest_err/rest_err.go
@@ -0,0 +1,74 @@
+package resterr_test
+
+import "net/http"
+
+type RestErr struct {
+	Message string   `json:"message"`
+	Err     string   `json:"error"`
+	Code    int      `json:"code"`
+	Causes  []Causes `json:"causes"` // Array que conterá os campos que estão preenchidos de forma incorreta
+}
+
+type Causes struct {
+	Field   string `json:"field"`
+	Message string `json:"message"`
+}
+
+//Criação de uma interface error para satisfazer as bibliotecas que recebem error por parametros
+// ou retornam error por parametros
+func (r *RestErr) Error() string {
+	return r.Message
+}
+
+// construtor do objeto de erros
+func NewRestErr(message, err string, code int, causes []Causes) *RestErr {
+	return &RestErr{
+		Message: message,
+		Err:     err,
+		Code:    code,
+		Causes:  causes,
+	}
+}
+
+//metodo para cada tipo de erro
+
+func NewBadRequestError(message string) *RestErr {
+	return &RestErr{
+		Message: message,
+		Err:     "bad_request",
+		Code:    http.StatusBadRequest,
+	}
+}
+
+func NewBadRequestValidationError(message string, causes []Causes) *RestErr {
+	return &RestErr{
+		Message: message,
+		Err:     "bad_request",
+		Code:    http.StatusBadRequest,
+		Causes:  causes,
+	}
+}
+
+func NewInternalServerError(message string) *RestErr {
+	return &RestErr{
+		Message: message,
+		Err:     "internal_server_error",
+		Code:    http.StatusInternalServerError,
+	}
+}
+
+func NewNotFoundError(message string) *RestErr {
+	return &RestErr{
+		Message: message,
+		Err:     "not_found",
+		Code:    http.StatusNotFound,
+	}
+}
+
+func NewForbiddenError(message string) *RestErr {
+	return &RestErr{
+		Message: message,
+		Err:     "forbidden",
+		Code:    http.StatusForbidden,
+	}
+}
