6d94b363864eed6dcf701dd1b4a17a81ea74a69f
Thiago <tfrancar@gmial.com>
2023-08-13 22:45:33 -0300

    importação do gonic

---
+++ src/controller/routers/routers.go
@@ -0,0 +1,15 @@
+package routers
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/tfrancar/meu-primeiro-crud-go/src/controller"
+)
+
+func InitRoutes(r *gin.RouterGroup) {
+
+	r.GET("/getUserById/:userId", controller.FindUserByID)
+	r.GET("/getUserByEmail/:userEmail", controller.FindUserByEmail)
+	r.POST("/createrUser", controller.CreateUser)
+	r.PUT("/updateUser/:userId", controller.UpdateUser)
+	r.DELETE("/deleteUser/:userId", controller.DeleteUser)
+}
