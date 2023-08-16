89ea7bf2293ed1190866d4d5a90e3e4fc987bfa0
Thiago <tfrancar@gmial.com>
2023-08-15 21:29:50 -0300

    Criação do user_resquest e user_response. Alteração do create_User

--- src/controller/create_User.go
+++ src/controller/create_User.go
@@ -1,12 +1,24 @@
 package controller
 
 import (
+	"fmt"
+
 	"github.com/gin-gonic/gin"
 	"github.com/tfrancar/meu-primeiro-crud-go/src/configuration/rest_err"
+	"github.com/tfrancar/meu-primeiro-crud-go/src/controller/model/request"
 )
 
 func CreateUser(c *gin.Context) {
 
-	err := rest_err.NewBadRequestError("Voce chamou a rota de forma errada")
-	c.JSON(err.Code, err)
+	var userRequest request.UserResquest
+
+	if err := c.ShouldBindJSON(&userRequest); err != nil {
+		restErr := rest_err.NewBadRequestError(
+			fmt.Sprintf("There are some incorrect filds, error=%s", err.Error()))
+
+		c.JSON(restErr.Code, restErr)
+		return
+	}
+
+	fmt.Println(userRequest)
 }
