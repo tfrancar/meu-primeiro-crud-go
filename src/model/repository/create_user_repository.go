0815437f1f19cf6ec0446a74ee6397b0001c21c7
tfrancar <tfrancar@gmail.com>
2023-08-27 19:36:40 -0300

    inclusão da interface para comunicação com os services.

--- src/model/repository/create_user_repository.go
+++ src/model/repository/create_user_repository.go
@@ -1,1 +1,36 @@
 package repository
+
+import (
+	"context"
+	"os"
+
+	"github.com/tfrancar/meu-primeiro-crud-go/src/configuration/logger"
+	"github.com/tfrancar/meu-primeiro-crud-go/src/configuration/rest_err"
+	"github.com/tfrancar/meu-primeiro-crud-go/src/model"
+)
+
+const (
+	MONGO_USER_DB = "MONGO_USER_DB"
+)
+
+func (ur *userRepository) createUser(
+	userDomain model.UserDomainInterface,
+) (model.UserDomainInterface, *rest_err.RestErr) {
+
+	logger.Info("Init createUser repository")
+	collection_name := os.Getenv(MONGO_USER_DB)
+
+	collection := ur.databaseConnection.Collection(collection_name)
+
+	value, err := userDomain.GetJSONValue()
+	if err != nil {
+		return nil, rest_err.NewInternalServerError(err.Error())
+	}
+	result, err := collection.InsertOne(context.Background(), value)
+	if err != nil {
+		return nil, rest_err.NewInternalServerError(err.Error())
+	}
+
+	userDomain.SetID(result.InsertedID.(string))
+	return userDomain, nil
+}
