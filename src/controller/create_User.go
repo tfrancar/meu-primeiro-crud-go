39649bc55a09d6fdf44793f4810e9affb9d1f9a6
tfrancar <tfrancar@gmail.com>
2023-08-27 21:45:41 -0300

    Coloção de logger.

--- src/controller/create_User.go
+++ src/controller/create_User.go
@@ -43,11 +43,21 @@ func (uc *userControllerInterface) CreateUser(c *gin.Context) {
 	// Repassa o domain criado ali em cima para o CreateUser.
 	domainResult, err := uc.service.CreateUser(domain)
 	if err != nil {
+		logger.Error(
+			"Error trying to call CreateUser service",
+			err,
+			zap.String("journey", "createUser"),
+		)
 		c.JSON(err.Code, err)
 	}
 
 	logger.Info("User created successfully",
-		zap.String("journey", "CreateUser"))
+		zap.String("userId", domainResult.GetID()),
+		zap.String("journey", "CreateUser"),
+	)
 
-	c.JSON(http.StatusOK, view.ConvertDomainToResponse(domainResult))
+	c.JSON(http.StatusOK, view.ConvertDomainToResponse(
+		domainResult,
+	),
+	)
 }
